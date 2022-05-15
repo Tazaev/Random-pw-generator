1db40fea9039ab568d1918ad33553d51bae6a259
Tazaev <tonymurtazaev@gmail.com>
2022-05-14 21:34:31 -0400

    redid variables for password inputs

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,17 @@
-// list of special characters -!@#$%^&*()_+-
-// list of numerical digits
+//list of special characters -!@#$%^&*()_+-
+//list of numerical digits
 //list of upper case characters
 //list of lowercase characters
-const number = [1,2,3,4,5,6,7,8,9,0];
-const symbols =["!","@","#","$","%","^","&","*","~"];
-const letters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
- 
+var characterlength = 8;
+var choicearray= [];
+var specialcharacterarray = ["!","@","#","$","%","^","&","*","~","<",">","?","/",];
+var lowercasearray = ["a","b","c","d","e","f","g","h","i","g","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var uppercasearray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var numberarray = ["1","2","3","4","5","6","7","8","9","0",];
 
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -26,11 +28,11 @@ function writePassword() {
 //use numbers?
 //use capital letters?
 //use lower case letters?
-function generate password(){
+function generatePassword(){
+console.log("hey you clicked the button")
+ // var minimumpasswordlength = 8;
 
-  var minimumpasswordlength = 8;
-
-  var maximumpasswordlength = 128;
+ // var maximumpasswordlength = 128;
   
   var passwordlength = prompt("enter desired password length");
   
@@ -48,9 +50,9 @@ function generate password(){
   var minimumspecialcharacters = "";
   
  //put together the password
-//combine the lists that were chosen
+ //combine the lists that were chosen
 
-var functionArray = {
+ var functionArray = {
   getNumbers: function(){
     return String.fromCharCode(math.floor(math.random() * 8 + 37));
   },
@@ -64,15 +66,52 @@ var functionArray = {
   },
 
   getspecialcharacters: function(){
-    return String.symbols[(math.floor(math.random() * symbols.length)]
+    return String.symbols[math.floor(math.random() * symbols.length)]
+  }
+};
+
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
   }
 
- 
- 
-  //pick a random character from the combined list and place it it the password up the chosen length
-  return "mypassword"
 
-}
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+
+  }
+
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCount++;
+
+  }
+ 
+  var randompasswordgenerated = "";
+
+  for (let i=0; i < (parseInt(passwordlength) - minimumcount); i++){
+    var randomnumberpicked = math.floor(math.random() * 4);
+  }
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+
+  //pick a random character from the combined list and place it it the password up the chosen length
+
+ 
+  return "mypassword"
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
